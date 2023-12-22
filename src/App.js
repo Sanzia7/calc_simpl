6e038d2e988c38628c963d1a4ad77dc2595699b9
Sanzia7 <meritee@gmx.de>
2023-12-22 17:14:38 +0100

    second variant of jjsx code

--- src/App.js
+++ src/App.js
@@ -1,94 +1,93 @@
 import { useState } from 'react'
 import styles from './App.module.css'
 
-const NUMS = [
-	{ id: '0', name: '0' },
-	{ id: '1', name: '1' },
-	{ id: '2', name: '2' },
-	{ id: '3', name: '3' },
-	{ id: '4', name: '4' },
-	{ id: '5', name: '5' },
-	{ id: '6', name: '6' },
-	{ id: '7', name: '7' },
-	{ id: '8', name: '8' },
-	{ id: '9', name: '9' },
-]
+const NUMS = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+const OPERATORS = ['+', '-', '=', 'C']
+const NUMBER = 'NUMBER'
+const OPERATOR = 'OPERATOR'
 
 export const App = () => {
-	let [operand1, setOperand1] = useState('')
-	let [operand2, setOperand2] = useState('')
-	let [operator, setOperator] = useState('')
-	let [result, setResult] = useState(false)
+	const [operand1, setOperand1] = useState('')
+	const [operand2, setOperand2] = useState('')
+	const [operator, setOperator] = useState('')
+	const [isResult, setIsResult] = useState(false)
 
-	function reset() {
-		setOperand1((operand1 = ''))
-		setOperand2((operand2 = ''))
-		setOperator((operator = ''))
+	const clear = () => {
+		setOperand1('')
+		setOperand2('')
+		setOperator('')
+		setIsResult(false)
 	}
 
-	function output(number) {
-		if (operand2 === '' && operator === '') {
-			setOperand1(String(operand1) + number)
+	const handleClick = (value, type) => () => {
+		if (type === NUMBER) {
+			if (operand1 && operator) {
+				setOperand2((prev) => prev + value)
+			} else {
+				setOperand1((prev) => prev + value)
+			}
+		}
+
+		if (type === OPERATOR) {
+			switch (value) {
+				case 'C':
+					clear()
+					break
+				case '=':
+					setIsResult(true)
+					break
+				default:
+					if (operand1) {
+						setOperator(value)
+					}
+			}
+		}
+
+		if (isResult) {
+			clear()
+			setIsResult(false)
+		}
+	}
+
+	const renderOutput = () => {
+		let output = ''
+
+		if (isResult) {
+			if (operator === '+') {
+				output = Number(operand1) + Number(operand2)
+			}
+
+			if (operator === '-') {
+				output = Number(operand1) - Number(operand2)
+			}
 		} else {
-			setOperand2(String(operand2) + number)
+			output = `${operand1}${operator}${operand2}`
 		}
-	}
 
-	function resulting(operand1, operator, operand2) {
-		if (operator === '+') {
-			setOperand1((operand1 = Number(operand1) + Number(operand2)))
-		} else if (operator === '-') {
-			setOperand1((operand1 = Number(operand1) - Number(operand2)))
-		}
-		setOperand2((operand2 = ''))
-		setOperator((operator = ''))
-		console.log('result')
-
+		return output
 	}
 
 	return (
 		<div className={styles.app}>
-			<h1>Simple Calculator</h1>
 			<div className={styles.container}>
-				<div className={result ? styles.outcome : styles.display}>
-					{String(operand1) + String(operator) + String(operand2) }
-				</div>
+				<div className={styles.display}>{renderOutput()} </div>
 				<div className={styles.operators}>
-					<button
-						className={styles.operand}
-						onClick={() => {
-							setOperator(operator + '+')
-							setResult((result = false))
-						}}
-					>
-						+
-					</button>
-					<button
-						className={styles.operand}
-						onClick={() => {
-							setOperator(operator + '-')
-							setResult((result = false))
-						}}
-					>
-						-
-					</button>
-					<button className={styles.operand} onClick={() => reset()}>
-						C
-					</button>
-					<button
-						className={styles.operand}
-						onClick={() => {
-							resulting(operand1, operator, operand2)
-							setResult((result = true))
-						}}
-					>
-						=
-					</button>
+					{OPERATORS.map((item) => (
+						<button
+							className={styles.operand}
+							key={item}
+							onClick={handleClick(item, OPERATOR)}>
+							{item}
+						</button>
+					))}
 				</div>
 				<div className={styles.nums}>
-					{NUMS.map((num) => (
-						<button key={num.id} className={styles.button} onClick={() => output(num.name)}>
-							{num.name}
+					{NUMS.map((item) => (
+						<button
+							key={item}
+							className={styles.button}
+							onClick={handleClick(item, NUMBER)}>
+							{item}
 						</button>
 					))}
 				</div>
@@ -96,3 +95,99 @@ export const App = () => {
 		</div>
 	)
 }
+
+// const NUMS = [
+// 	{ id: '0', name: '0' },
+// 	{ id: '1', name: '1' },
+// 	{ id: '2', name: '2' },
+// 	{ id: '3', name: '3' },
+// 	{ id: '4', name: '4' },
+// 	{ id: '5', name: '5' },
+// 	{ id: '6', name: '6' },
+// 	{ id: '7', name: '7' },
+// 	{ id: '8', name: '8' },
+// 	{ id: '9', name: '9' },
+// ]
+//
+// export const App = () => {
+// 	let [operand1, setOperand1] = useState('')
+// 	let [operand2, setOperand2] = useState('')
+// 	let [operator, setOperator] = useState('')
+// 	let [result, setResult] = useState(false)
+//
+// 	function reset() {
+// 		setOperand1((operand1 = ''))
+// 		setOperand2((operand2 = ''))
+// 		setOperator((operator = ''))
+// 	}
+//
+// 	function output(number) {
+// 		if (operand2 === '' && operator === '') {
+// 			setOperand1(String(operand1) + number)
+// 		} else {
+// 			setOperand2(String(operand2) + number)
+// 		}
+// 	}
+//
+// 	function resulting(operand1, operator, operand2) {
+// 		if (operator === '+') {
+// 			setOperand1((operand1 = Number(operand1) + Number(operand2)))
+// 		} else if (operator === '-') {
+// 			setOperand1((operand1 = Number(operand1) - Number(operand2)))
+// 		}
+// 		setOperand2((operand2 = ''))
+// 		setOperator((operator = ''))
+// 		console.log('result')
+//
+// 	}
+//
+// 	return (
+// 		<div className={styles.app}>
+// 			<h1>Simple Calculator</h1>
+// 			<div className={styles.container}>
+// 				<div className={result ? styles.outcome : styles.display}>
+// 					{String(operand1) + String(operator) + String(operand2) }
+// 				</div>
+// 				<div className={styles.operators}>
+// 					<button
+// 						className={styles.operand}
+// 						onClick={() => {
+// 							setOperator(operator + '+')
+// 							setResult((result = false))
+// 						}}
+// 					>
+// 						+
+// 					</button>
+// 					<button
+// 						className={styles.operand}
+// 						onClick={() => {
+// 							setOperator(operator + '-')
+// 							setResult((result = false))
+// 						}}
+// 					>
+// 						-
+// 					</button>
+// 					<button className={styles.operand} onClick={() => reset()}>
+// 						C
+// 					</button>
+// 					<button
+// 						className={styles.operand}
+// 						onClick={() => {
+// 							resulting(operand1, operator, operand2)
+// 							setResult((result = true))
+// 						}}
+// 					>
+// 						=
+// 					</button>
+// 				</div>
+// 				<div className={styles.nums}>
+// 					{NUMS.map((num) => (
+// 						<button key={num.id} className={styles.button} onClick={() => output(num.name)}>
+// 							{num.name}
+// 						</button>
+// 					))}
+// 				</div>
+// 			</div>
+// 		</div>
+// 	)
+// }
